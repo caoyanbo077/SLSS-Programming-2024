451fe61aa0ff9720ff2e25076110ffd66582988d
Jimmyyybc <caojim2@gmail.com>
2024-04-16 12:47:53 -0700

    bb

---
+++ notes-2-4-dictionaries.py
@@ -0,0 +1,15 @@
+# Dictionaries 
+# 12 April 2024 
+
+person = ["John", "23 years old","Likes bananas"]
+print (person[1])
+print(person[2])
+
+person_dict = {
+    "name": "John",
+    "age": "23",
+    "likes": "bananas"
+
+}
+print(person_dict["age"])
+print(person_dict["name"])
--- notes-2.5.py
+++ notes-2.5.py
@@ -1,28 +1,99 @@
-class Pokemon: 
-    def _int_(self):
-        """Constructor: contains all properties of a pokemon
+# Classes and Objects
+
+# Big Ideas:
+#   - Classes allow us to couple data and functions together
+
+
+# Create a class that represents a Pokemon
+class Pokemon:  # always name classes with capital
+    def __init__(self):
+        """Constructor: contains all properties
+        of a Pokemon. It also contains the default
+        state of the properties.
         """
-        self.name = " "
+        self.name = ""
         self.id = 0
-        self.weight = 0 
-        self.height = 0 
+        self.weight = 0
+        self.height = 0
         self.type = "Normal"
-    pass
+        self.actual_cry = "Rooooooooooooooar"
+
+    def cry(self) -> str:
+        """Represents the sound a Pokemon makes
+
+        Returns:
+            - As a string the sound a pokemon makes
+            - e.g. "{name} says, "{actual_cry}"""
+
+        return f'{self.name} says, "{self.actual_cry}!"'
+
+    def consume(self, item: str) -> str:
+        """Pokemon consumes the item
+
+        Params:
+            - the item the pokemon consumes
+
+        Returns:
+            - the response of the pokemon
+        """
+        if item.lower() == "berry":
+            return f'{self.name} eats the berry and says, "YUM!"'
+        elif item.lower() == "potion":
+            return f"{self.name} feels much better after the potion!"
+        else:
+            return f"{self.name} batted away the {item}!"
+
+class Pikachu(Pokemon):
+    def __init__(self):
+        super().__init__()
 
 def main():
+    # Create two Pokemon
+
+    # Create something 'Pikachu'-like
     pokemon_one = Pokemon()
+
+    # Access the properties inside pokemon_one
+    # Print the name of pokemon_one
+    print(pokemon_one.name)
+    print(pokemon_one.type)
+
+    # Change the values of the propeties
     pokemon_one.name = "Pikachu"
     pokemon_one.type = "Electric"
     pokemon_one.id = 25
+
+    # Print the values from pokemon_one
     print(pokemon_one.name)
-    print(pokemon_one.id)
     print(pokemon_one.type)
+
+    # TODO: Create something 'Squirtle'-like
+    #  - Create a new Pokemon object
+    #      - Store this in variable pokemon_two
+    #  - Squirtle's Pokedex id is 4
+    #  - Squirtle's type is Water
 
     pokemon_two = Pokemon()
     pokemon_two.name = "Squirtle"
-    pokemon_two.id = 4
     pokemon_two.type = "Water"
+    pokemon_two.id = 4
+
+    # To test, print out all of squirtle's properties
     print(pokemon_two.name)
+    print(pokemon_two.id)
+    print(pokemon_two.type)
+
+    # Test Pokemon cry
+    print(pokemon_one.cry())
+    print(pokemon_two.cry())
+
+    # Test Pokemon consume
+    print(pokemon_one.consume("berry"))
+    print(pokemon_one.consume("potion"))
+    print(pokemon_one.consume("poison"))  # mr. ubial doesn't condone
+    print(pokemon_two.consume("berry"))
+
+
 if __name__ == "__main__":
     main()
 
